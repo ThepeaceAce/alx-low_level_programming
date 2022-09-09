46526266290dbe3210845006e712717cc06c6b74
ThepeaceAce <peaceisace@gmail.com>
2022-09-08 18:08:33 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 /**
  * main - main block
+ * Return: 1
  */
 int main(void)
 {
-	int s =strlen("and that piece of art is useful\" - Dora Kopar,	2015-10-19\n");
+	int s =	strlen("and that piece of art is useful\" - Dora Kopar,	2015-10-19\n");
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
 	return (1);
 }
